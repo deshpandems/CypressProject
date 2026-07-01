2c13454f707fd213ab02d8b310cd24010d9f7ca8
Mahesh Deshpande <deshpandems.28@gmail.com>
2026-07-01 19:49:47 +0530

    All cases are working fine and all the test cases are completed successfully.

--- cypress/e2e/Automation_exercise_dot_com/Test-case-7.cy.js
+++ cypress/e2e/Automation_exercise_dot_com/Test-case-7.cy.js
@@ -3,7 +3,7 @@ describe('Verify Test Cases Page', () => {
         // Verify that home page is visible successfully
         cy.visit('https://automationexercise.com/')
         cy.title().should('contain', 'Automation Exercise')
-
+        //aaaddbdbqwdhqbdqj
         //click on 'Test Cases' button
         cy.xpath("//header//a[@href='/test_cases']").click();
         cy.url().should('eq', 'https://automationexercise.com/test_cases');
